cd58343daa1cf0a524bebf2e0b625ab1ca4e0798
Roman Shelekhov <shelekhoff@gmail.com>
2024-10-09 14:02:53 +0400

    add sso grpc client

---
+++ client.go
@@ -0,0 +1,63 @@
+package sso_grpc_client
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"time"
+
+	grpclog "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
+	grpcretry "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/retry"
+	ssov1 "github.com/rshelekhov/sso-protos/gen/go/sso"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	Api    ssov1.AuthClient
+	logger *slog.Logger
+}
+
+func New(
+	log *slog.Logger,
+	addr string,
+	timeout time.Duration,
+	retriesCount int,
+) (*Client, error) {
+	const op = "sso.grpc.New"
+
+	retryOpts := []grpcretry.CallOption{
+		grpcretry.WithCodes(codes.NotFound, codes.Aborted, codes.DeadlineExceeded),
+		grpcretry.WithMax(uint(retriesCount)),
+		grpcretry.WithPerRetryTimeout(timeout),
+	}
+
+	logOpts := []grpclog.Option{
+		grpclog.WithLogOnEvents(grpclog.PayloadReceived, grpclog.PayloadSent),
+	}
+
+	// TODO: updated to use secure transport
+
+	cc, err := grpc.NewClient(addr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithChainUnaryInterceptor(
+			grpclog.UnaryClientInterceptor(interceptorLogger(log), logOpts...),
+			grpcretry.UnaryClientInterceptor(retryOpts...),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Client{
+		Api:    ssov1.NewAuthClient(cc),
+		logger: log,
+	}, nil
+}
+
+func interceptorLogger(l *slog.Logger) grpclog.Logger {
+	return grpclog.LoggerFunc(func(ctx context.Context, lvl grpclog.Level, msg string, fields ...any) {
+		l.Log(ctx, slog.Level(lvl), msg, fields...)
+	})
+}
---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/rshelekhov/sso-grpc-client
+
+go 1.23.0
+
+require (
+	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.1.0
+	github.com/rshelekhov/sso-protos v0.0.35
+	google.golang.org/grpc v1.67.1
+)
+
+require (
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241007155032-5fefd90f89a9 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
+)
